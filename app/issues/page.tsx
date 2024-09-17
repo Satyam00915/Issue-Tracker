72e31ea21dd238f197cfb897a41a05a910937682
Satyam Upadhyay <satyamupadhyay00915@gmail.com>
2024-09-17 20:03:05 +0530

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
-
+import { Button } from "@radix-ui/themes";
 const page = () => {
   return (
     <div className="text-black">
       Issues
+      <Button>eamfl</Button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
